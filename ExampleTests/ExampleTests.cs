2aea24f548fa11e3d827b44f48b7a99b0387dc80
Payne325 <alex.payne325@gmial.com>
2019-04-03 22:01:11 +0100

    Initial Commit, writes to console but not file. Does not split numbers yet.

---
+++ ExampleTests/ExampleTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExampleTests
+{
+    [TestClass]
+    public class ExampleTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        protected void GivenPaynoWhenInsanoThenHeIsTheGreatestO()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ TestNameParser/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNameParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool parseLine = false;
+
+            foreach (var file in args)
+            {
+                //Extract all lines
+                var lines = File.ReadAllLines(file);
+
+                //Iterate through the lines
+
+                foreach (var line in lines)
+                {
+                    if(parseLine)
+                    {
+                        //Parse the line, remove protection modifiers, return types and parentheses.
+                        var splitLine = line.Split(' ');
+
+                        var words = splitLine.Where(x => !string.IsNullOrEmpty(x));
+
+                        var testName = words.ElementAt(2);
+
+                        testName = testName.Remove(testName.Length - 2);
+                        testName = SplitForCamelCase(testName);
+
+                        Console.Out.Write(testName + "\n");
+
+                        //Reset parse line flag 
+                        parseLine = false;
+                    }
+                    else if(LineContainsTestTag(line))
+                    {
+                        //If line contains text tag, then we will want to parse the next line.
+                        parseLine = true;
+                    }
+                }
+            }
+
+            Console.Out.Write("Test Case Extraction Complete! Press any button to exit the program...");
+            Console.ReadKey();
+        }
+
+        static bool LineContainsTestTag(String line)
+        {
+            return line.Contains("[TestMethod]");
+        }
+
+        static string SplitForCamelCase(string testName)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(
+                testName, 
+                "([A-Z])", 
+                " $1", 
+                System.Text.RegularExpressions.RegexOptions.Compiled)
+                .Trim();
+        }
+    }
+}
